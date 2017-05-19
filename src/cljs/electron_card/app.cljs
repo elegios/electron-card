4efeb0955ba38c6902888c3d5e7c05a20197a19e
Viktor Palmkvist <viktor@palmkvist.com>
2017-05-19 21:24:19 +0200

    Working live reloading, html and css generation in electron

--- src/cljs/electron_card/app.cljs
+++ src/cljs/electron_card/app.cljs
@@ -2,7 +2,9 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs.nodejs :as nodejs]
             [electron-card.eval :as ev]
-            [cljs.core.async :refer [<! promise-chan put!]]))
+            [cljs.core.async :refer [<! promise-chan put!]]
+            [garden.core :refer [css]]
+            [hipo.core :as hipo]))
 
 (nodejs/enable-util-print!)
 
@@ -15,6 +17,17 @@
     ret))
 
 (def last-source (atom ""))
+(def components-html (atom nil))
+
+(defn render-result [{c :css h :html}]
+  (let [comp-style (js/document.getElementById "components-style")
+        comp-html [:div#components-html h]]
+    (set! (.-innerHTML comp-style) (apply css c))
+    (if @components-html
+      (hipo/reconciliate! @components-html comp-html)
+      (do
+        (reset! components-html (hipo/create comp-html))
+        (js/document.body.appendChild @components-html)))))
 
 (defn file-changed [kind file]
   (if (not= kind "change")
@@ -26,7 +39,10 @@
           (do
             (println "different source")
             (reset! last-source new-source)
-            (println (<! (ev/eval new-source)))))))))
+            (let [{:keys [errors value]} (<! (ev/eval new-source))]
+              (if value
+                (render-result value)
+                (println errors)))))))))
 
 (defn init
   []
